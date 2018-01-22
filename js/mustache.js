2d0033333db02a58ac3315d0d21f74248bb1c698
Unknown <carlosmarquezlorenzo@hotmail.com>
2018-01-22 16:38:21 +0100

    Mustache

---
+++ js/mustache.js
@@ -0,0 +1,19 @@
+
+var pepito = {
+
+nombre: "Carlos",
+direccion: "Pitas",
+NIF: 5794375,
+telefono: 5654637,
+email: "jnhgfujh@gmail.com"
+
+};
+
+function cargarDatos(){
+            var datos = Mustache.render("<strong>{{nombre}}</strong><br><strong>{{direccion}}</strong><br><strong>{{NIF}}</strong><br><strong>{{telefono}}</strong><br><strong>{{email}}</strong><br>", pepito);
+            $('#mustachote').html(datos);
+        }
+ 
+        $( document ).ready(function() {
+            cargarDatos();
+        });
