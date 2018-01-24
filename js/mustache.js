0c2b9053ab8c49285799ee28c2c4bdaa56fcca76
Unknown <carlosmarquezlorenzo@hotmail.com>
2018-01-24 16:33:50 +0100

    Handlebar

--- js/mustache.js
+++ js/mustache.js
@@ -9,11 +9,18 @@ email: "jnhgfujh@gmail.com"
 
 };
 
-function cargarDatos(){
-            var datos = Mustache.render("<strong>{{nombre}}</strong><br><strong>{{direccion}}</strong><br><strong>{{NIF}}</strong><br><strong>{{telefono}}</strong><br><strong>{{email}}</strong><br>", pepito);
-            $('#mustachote').html(datos);
-        }
+// function cargarDatos(){
+//             var datos = Mustache.render("<strong>{{nombre}}</strong><br><strong>{{direccion}}</strong><br><strong>{{NIF}}</strong><br><strong>{{telefono}}</strong><br><strong>{{email}}</strong><br>", pepito);
+//             $('#mustachote').html(datos);
+//         }
  
-        $( document ).ready(function() {
-            cargarDatos();
-        });
+//         $( document ).ready(function() {
+//             cargarDatos();
+//         });
+alert("hola");
+var pepe = $( "#lista" ).html();
+var compilar = Handlebars.compile(pepe);
+var html = compilar(pepito);
+$("#resultado").html(html);
+
+
